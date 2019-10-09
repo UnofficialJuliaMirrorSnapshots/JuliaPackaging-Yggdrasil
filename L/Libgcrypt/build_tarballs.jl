acc2915683e0902616534f561503b3b6a3854e7b
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-09 13:02:58 +0000

    Snapshot of branch master taken on 2019-10-09T09:02:56.541-04:00 from "https://github.com/UnofficialJuliaMirror/JuliaPackaging-Yggdrasil"

--- L/Libgcrypt/build_tarballs.jl
+++ L/Libgcrypt/build_tarballs.jl
@@ -2,40 +2,38 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-name = "x265"
-version = v"3.0"
+name = "Libgcrypt"
+version = v"1.8.5"
 
-# Collection of sources required to build x265Builder
+# Collection of sources required to build libgcrypt
 sources = [
-    "http://ftp.videolan.org/pub/videolan/x265/x265_3.0.tar.gz" =>
-    "c5b9fc260cabbc4a81561a448f4ce9cad7218272b4011feabc3a6b751b2f0662",
+    "https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-$(version).tar.bz2" =>
+    "3b4a2a94cb637eff5bdebbcaf46f4d95c4f25206f459809339cdada0eb577ac3",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/x265*/
-
-# We need `nasm` for x86_64
-apk add nasm
-
-mkdir bld && cd bld
-cmake -DCMAKE_INSTALL_PREFIX="${prefix}" -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" -DENABLE_PIC=ON -DENABLE_SHARED=ON ../source
+cd $WORKSPACE/srcdir/libgcrypt-*/
+./configure --prefix=${prefix} --host=${target} \
+    --disable-padlock-support \
+    --disable-asm
 make -j${nproc}
 make install
 """
 
 # These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
+# platforms are passed in on the command line.  We are manually disabling
+# many platforms that do not seem to work.
 platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
-    ExecutableProduct("x265", :x265),
-    LibraryProduct("libx265", :libx265)
+    LibraryProduct("libgcrypt", :libgcrypt),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    "Libgpg_error_jll"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
---
+++ L/Libgpg_error/build_tarballs.jl
@@ -0,0 +1,58 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg.BinaryPlatforms
+
+name = "Libgpg_error"
+version = v"1.36"
+
+# Collection of sources required to build Libgpg-Error
+sources = [
+    "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-$(version.major).$(version.minor).tar.bz2" =>
+    "babd98437208c163175c29453f8681094bcaf92968a15cafb1a276076b33c97c",
+    "./bundled",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/libgpg-error-*/
+
+for p in ${WORKSPACE}/srcdir/patches/*; do
+    atomic_patch -p1 "${p}"
+done
+
+# "Generate" new arch platform definitions because they're actually really simple. :P
+cp -iv src/syscfg/lock-obj-pub.i686-unknown-linux-gnu.h src/syscfg/lock-obj-pub.i686-unknown-linux-musl.h
+cp -iv src/syscfg/lock-obj-pub.aarch64-unknown-linux-gnu.h src/syscfg/lock-obj-pub.aarch64-unknown-linux-musl.h
+cp -iv src/syscfg/lock-obj-pub.arm-unknown-linux-gnueabi.h src/syscfg/lock-obj-pub.arm-unknown-linux-musleabihf.h
+cp -iv src/syscfg/lock-obj-pub.x86_64-unknown-linux-gnu.h src/syscfg/lock-obj-pub.freebsd11.1.h
+
+# Use libgpg-specific mapping for triplets
+TARGET="${target}"
+if [[ "${target}" == x86_64-apple-darwin* ]]; then
+    TARGET=x86_64-apple-darwin
+fi
+
+# We need `msgformat`
+apk add gettext
+
+./configure --prefix=${prefix} --host=${TARGET} --build=${MACHTYPE}
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line.  We are manually disabling
+# many platforms that do not seem to work.
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct(["libgpg-error", "libgpg-error6"], :libgpg_error),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
